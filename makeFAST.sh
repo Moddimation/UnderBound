bfa0d692f55f507fe337a4e920f9f519d54a2430
Dmytro Vakhitov <vakhdmyt06@gmail.com>
2023-01-07 17:04:52 +0100

    Build Quick

---
+++ makeFAST.sh
@@ -0,0 +1,4 @@
+"C:\ProgramData\GameMakerStudio2-LTS\Cache\runtimes\runtime-2022.0.0.12/bin/igor/windows/x64/Igor.exe"  -j=8 -options=BUILD/buildwinfast.bff -v -- Windows PackageZip
+git add .
+git commit -m "Build Quick"
+git push
